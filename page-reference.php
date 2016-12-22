6e44bb3e80ceabe89153be2c09faa507414739cb
tmontdev <the.monteiro.dev@gmail.com>
2016-12-22 00:32:31 -0200

    see you tomorrow

--- page-reference.php
+++ page-reference.php
@@ -1,5 +1,6 @@
 <section class="page-reference">
-  <?php $pagename = get_query_var('pagename');
+  <?php
+      $pagename = get_query_var('pagename');
       if ( !$pagename && $id > 0 ) {
       // If a static page is set as the front page, $pagename will not be set. Retrieve it from the queried object
       $post = $wp_query->get_queried_object();
@@ -10,7 +11,6 @@
     <div class="container">
         <a href="<?php echo get_option('home'); ?>" class="home-icon text-green fa fa-home" aria-hidden="true"></a>
         <a class="pr-page text-grey" href="<?php get_page_by_path($pagename); ?>">  <?php echo $pagename ?></a>
-        <a class="pr-post text-grey" href="<?php echo get_option('home'); ?>"><?php echo  $post ?></a>
     </div>
   </div>
 </section>
