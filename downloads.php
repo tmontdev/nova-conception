267f7774dae074e8fcfcab1f36cc7d9fabc09d36
Marcos Felipe <omarkdev@gmail.com>
2017-06-05 20:05:52 -0300

    aray test

--- downloads.php
+++ downloads.php
@@ -53,10 +53,10 @@ include(get_template_directory()."/page-reference.php"); ?>
                 while ($downloads->have_posts()):
                     $downloads->the_post();
 
-                    $file = get_field('arquivo');
+                    $fileDownload = get_field('arquivo');
                     ?>
                     <div class="post-field col-xs-12 col-md-4">
-                        <a href="<?php echo $file->url; ?>" download>
+                        <a href="<?php echo $fileDownload['url']; ?>" download>
                             <div class="post-content bg-white whole-block">
                                 <div class="post-icon">
                                     <span class="fa fa-file-word-o text-center whole-block"></span>
@@ -66,10 +66,10 @@ include(get_template_directory()."/page-reference.php"); ?>
                                 </div>
                                 <div class="post-description">
                                     <i>
-                                        <?php echo $file->description; ?>
+                                        <?php echo $fileDownload['description']; ?>
                                     </i>
                                 </div>
-                                <div class="post-size"><?php filesize( get_attached_file( $file->ID ) ); ?></div>
+                                <div class="post-size"></div>
                                 <div class="post-button"><span class="fa fa-download"> <span class="button-text">Download</span></div>
                             </div>
                         </a>
