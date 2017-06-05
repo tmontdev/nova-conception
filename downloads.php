e0ba4fbfb8b0fc9fe3a98f49c1a41aa5e833da28
Marcos Felipe <omarkdev@gmail.com>
2017-06-05 20:09:40 -0300

    jsut the_field

--- downloads.php
+++ downloads.php
@@ -52,11 +52,9 @@ include(get_template_directory()."/page-reference.php"); ?>
                 <?php
                 while ($downloads->have_posts()):
                     $downloads->the_post();
-
-                    $fileDownload = get_field('arquivo', get_the_ID());
                     ?>
                     <div class="post-field col-xs-12 col-md-4">
-                        <a href="<?php echo $fileDownload['url']; ?>" download>
+                        <a href="<?php the_post('arquivo'); ?>" download>
                             <div class="post-content bg-white whole-block">
                                 <div class="post-icon">
                                     <span class="fa fa-file-word-o text-center whole-block"></span>
@@ -66,7 +64,7 @@ include(get_template_directory()."/page-reference.php"); ?>
                                 </div>
                                 <div class="post-description">
                                     <i>
-                                        <?php echo $fileDownload['description']; ?>
+                                        
                                     </i>
                                 </div>
                                 <div class="post-size"></div>
