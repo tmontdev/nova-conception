ae5b6b7f2b85dc06a744a7aa809879962e4ffd13
Marcos Felipe <omarkdev@gmail.com>
2017-06-06 19:25:51 -0300

    show posts

--- downloads.php
+++ downloads.php
@@ -4,10 +4,30 @@
 */
 get_header();
 
-$downloads = new WP_Query([
+$args = [
     'post_type' => 'download',
     'posts_per_page' => '-1'
-]);
+];
+
+$search = isset($_GET['s']) ? trim($_GET['s']) : null;
+
+if ($search) {
+    $args['s'] = $search;
+}
+
+$downloads = new WP_Query($args);
+
+$downloadPosts = [];
+while ($downloads->have_posts()){
+    $downloads->the_post();
+
+    $downloadPosts[] = [
+        'title' => get_the_title(),
+        'description' => get_the_content(),
+        'size' => filesize(get_field('arquivo')),
+        'file' => get_field('arquivo')
+    ];
+}
 ?>
 <?php 
 $imgPage = do_shortcode('[easy_options id="DownloadsBanner"]');
@@ -15,26 +35,33 @@ include(get_template_directory()."/page-reference.php"); ?>
 
 <section class="search-bar whole-block bg-green">
 	<div class="container">
-		<div class="row">
-			<div class="text-search-field col-xs-12 col-md-6">
-				<input type="text" class="whole-block text-search" placeholder="Digite o que deseja Pesquisar!">
-				<div class="send-button-field">
-					<button>
-						<a href="#" class="send-button">
-							<span class="fa fa-search"><span class="button-text">Pesquisar</span></span>
-						</a>
-					</button>
-				</div>
-			</div>
-			<div class="category-search-field col-xs-12 col-md-6">
-				<select name="category-selection" class="whole-block" multiple="multiple" id="category-selection">
-					<option value="Consultoria">Consultoria</option>
-					<option value="Combate ao Incendio"> Combate ao Incendio</option>
-					<option value="Segurança do Trabalho">Segurança do Trabalho</option>
-					<option value="Eletricidade">Eletricidade</option>
-				</select>
-			</div>
-		</div>
+        <form action="<?php
+        global $wp;
+        $current_url = home_url(add_query_arg(array(),$wp->request));
+
+        echo $current_url
+        ?>" method="post">
+            <div class="row">
+                <div class="text-search-field col-xs-12 col-md-6">
+                    <input type="text" name="s" class="whole-block text-search" placeholder="Digite o que deseja Pesquisar!">
+                    <div class="send-button-field">
+                        <button>
+                            <a href="#" class="send-button">
+                                <span class="fa fa-search"><span class="button-text">Pesquisar</span></span>
+                            </a>
+                        </button>
+                    </div>
+                </div>
+                <div class="category-search-field col-xs-12 col-md-6">
+                    <select name="category-selection" class="whole-block" multiple="multiple" id="category-selection">
+                        <option value="Consultoria">Consultoria</option>
+                        <option value="Combate ao Incendio"> Combate ao Incendio</option>
+                        <option value="Segurança do Trabalho">Segurança do Trabalho</option>
+                        <option value="Eletricidade">Eletricidade</option>
+                    </select>
+                </div>
+            </div>
+        </form>
 	</div>
 </section>
 
@@ -50,67 +77,29 @@ include(get_template_directory()."/page-reference.php"); ?>
 		<div class="whole-block downloads-square">
 			<div class="row downloads-row">
                 <?php
-                while ($downloads->have_posts()):
-                    $downloads->the_post();
+                foreach($downloadPosts as $downloadPost) {
                     ?>
                     <div class="post-field col-xs-12 col-md-4">
-                        <a href="<?php the_field('arquivo'); ?>" download>
+                        <a href="<?php echo $downloadPost['file']; ?>" download>
                             <div class="post-content bg-white whole-block">
                                 <div class="post-icon">
                                     <span class="fa fa-file-word-o text-center whole-block"></span>
                                 </div>
                                 <div class="post-title">
-                                    <h6 class="text-title"><?php the_title(); ?></h6>
+                                    <h6 class="text-title"><?php echo $downloadPost['title']?></h6>
                                 </div>
                                 <div class="post-description">
-                                    <i>
-                                    </i>
+                                    <i><?php echo $downloadPost['description']; ?></i>
                                 </div>
-                                <div class="post-size"><?php filesize(get_the_field('arquivo')); ?></div>
-                                <div class="post-button"><span class="fa fa-download"> <span class="button-text">Download</span></div>
+                                <div class="post-size"><?php echo $downloadPost['size']; ?></div>
+                                <div class="post-button"><span class="fa fa-download"> <span class="button-text">Download</span>
+                                </div>
                             </div>
                         </a>
                     </div>
                     <?php
-                endwhile;
+                }
                 ?>
-
-                        <div class="post-field col-xs-12 col-md-4">
-	              			<a href="#" target="_blank">
-	              				<div class="post-content bg-white whole-block">
-	              					<div class="post-icon">
-	              						<span class="fa fa-file-pdf-o text-center whole-block"></span>
-	              					</div>
-	              					<div class="post-title">
-	              						<h6 class="text-title">Título da Postagem Maior</h6>
-	              					</div>
-	              					<div class="post-description">
-	              						<i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum saepe earum mollitia vero est ullam, placeat eum rem deleniti sit quidem dolore.</i>
-	              					</div>
-	              					<div class="post-size">1.785Kb</div>
-	              					<div class="post-button"><span class="fa fa-download"> <span class="button-text">Download</span></div>
-	              				</div>
-	              			</a>
-	            			</div>
-	            			<div class="post-field col-xs-12 col-md-4">
-	              			<a href="#" target="_blank">
-	              				<div class="post-content bg-white whole-block">
-	              					<div class="post-icon">
-	              						<span class="fa fa-file-excel-o text-center whole-block"></span>
-	              					</div>
-	              					<div class="post-title">
-	              						<h6 class="text-title">Título da Postagem Maior</h6>
-	              					</div>
-	              					<div class="post-description">
-	              						<i>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum saepe earum mollitia vero est ullam, placeat eum rem deleniti sit quidem dolore.</i>
-	              					</div>
-	              					<div class="post-size">1.785Kb</div>
-	              					<div class="post-button"><span class="fa fa-download"> <span class="button-text">Download</span></div>
-	              				</div>
-	              			</a>
-	            			</div>
-
-            			</div>
 
             		</div>
             	</div>
