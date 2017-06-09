595ba6f8041ec4e671ea52195bf7d7c3107bb45a
Marcos Felipe <omarkdev@gmail.com>
2017-06-08 23:41:45 -0300

    about fix post-type and show

--- about.php
+++ about.php
@@ -17,7 +17,7 @@ $posts = array(
 );
 
 $args = array(
-  'post_type' => 'Sobre',
+  'post_type' => 'about',
     'posts_per_page' => 1
 );
 $query = new WP_Query( $args );
@@ -61,116 +61,68 @@ wp_reset_query();
   <div class="about-item hb-green text-green actived" id="company">
     <div class="about-item-bg desktop"></div>
     <div class="container">
-      <div class="col-xs-12 col-md-7 about-item-content">
         <?php foreach ($posts['company'] as $post): ?>
-        <div class="about-item-title">
-          <h2 class="text-title"><?php echo $post['title']; ?></h2>
-          <p><?php echo $post['description']; ?></p>
-        </div>
-      </div>
+          <div class="col-xs-12 col-md-7 about-item-content">
+
+            <div class="about-item-title">
+              <h2 class="text-title"><?php echo $post['title']; ?></h2>
+              <p><?php echo $post['description']; ?></p>
+            </div>
+          </div>
+        <?php endforeach;  ?>
     </div>
   </div>
-<?php endforeach;  ?>
+
   <div class="about-item hb-blue text-blue" id="mission">
     <div class="about-item-bg desktop"></div>
     <div class="container">
-      <div class="col-xs-12 col-md-7 about-item-content">
-        <div class="about-item-title">
-          <h2 class="text-title">Missão</h2>
-          <p>Ser referência no mercado de engenharia de projetos e consultoria, através do nosso profissionalismo, visando o aprimoramento continuo da gestão do nosso negócio, oferencendo sempre, técnicamente, economicamente e tecnológicamente,
-             o que há de melhor aos nossos clientes.</p>
-        </div>
-      </div>
+        <?php foreach ($posts['mission'] as $post): ?>
+            <div class="col-xs-12 col-md-7 about-item-content">
+                <div class="about-item-title">
+                    <h2 class="text-title"><?php echo $post['title']; ?></h2>
+                    <p><?php echo $post['description']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
   </div>
   <div class="about-item hb-green text-green" id="vision">
     <div class="about-item-bg desktop"></div>
     <div class="container">
-      <div class="col-xs-12 col-md-7 about-item-content">
-        <div class="about-item-title">
-          <h2 class="text-title">Visão</h2>
-          <p>Entender as necessidades de nossos clientes aplicando a engenharia de forma ética, técnica e eficaz, proporcionando assim qualidade, viabilidade e economia em cada trabalho concluído, contribuindo significativamente para o sucesso de cada
-            empreendimento construído.</p>
-        </div>
-      </div>
+      <?php foreach ($posts['vision'] as $post): ?>
+            <div class="col-xs-12 col-md-7 about-item-content">
+                <div class="about-item-title">
+                    <h2 class="text-title"><?php echo $post['title']; ?></h2>
+                    <p><?php echo $post['description']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
   </div>
   <div class="about-item hb-blue text-blue" id="values">
     <div class="about-item-bg desktop"></div>
     <div class="container">
-      <div class="col-xs-12 col-md-7 about-item-content">
-        <div class="about-item-title">
-          <h2 class="text-title">Valores</h2>
-          <ul>
-            <li>Agir de forma ética e sustentável</li>
-            <li>Promessa é dívida</li>
-            <li>Progredir sempre</li>
-            <li>Respeitar o ser humano</li>
-            <li>Ter compromisso com alto desempenho</li>
-            <li>Gestão prática</li>
-            <li>Organização dinâmica</li>
-            <li>Transpaência nos objetivos</li>
-            <li>Garra e vontade de vencer</li>
-          </ul>
-        </div>
-      </div>
+        <?php foreach ($posts['values'] as $post): ?>
+            <div class="col-xs-12 col-md-7 about-item-content">
+                <div class="about-item-title">
+                    <h2 class="text-title"><?php echo $post['title']; ?></h2>
+                    <p><?php echo $post['description']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
   </div>
   <div class="about-item hb-green text-green" id="partners">
     <div class="about-item-bg desktop"></div>
     <div class="container">
-      <div class="col-xs-12 col-md-7 about-item-content">
-        <div class="about-item-title">
-          <h2 class="text-title">Clientes e Parceiros</h2>
-          <div class="logo-square">
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/01.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/1.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/2.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/3.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/4.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/5.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/6.jpg">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/7.png" class="lower">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/8.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/9.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/10.jpg">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/11.jpg">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/12.png">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/13.jpg">
-              </div>
-              <div class="logo-img col-xs-6 col-sm-4 col-ld-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/partners/14.jpg">
-              </div>
-           </div>
-        </div>
-      </div>
+        <?php foreach ($posts['partners'] as $post): ?>
+            <div class="col-xs-12 col-md-7 about-item-content">
+                <div class="about-item-title">
+                    <h2 class="text-title"><?php echo $post['title']; ?></h2>
+                    <p><?php echo $post['description']; ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
   </div>
 </section>
