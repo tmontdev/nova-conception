283a6f8e3131bed352b124429f2efb4fe8099a33
Marcos Felipe <omarkdev@gmail.com>
2017-06-19 23:51:50 -0300

    correctly foreach

--- about.php
+++ about.php
@@ -74,10 +74,10 @@ wp_reset_query();
   </div>
 
   <div class="about-item hb-blue text-blue" id="mission">
-  <?php foreach ($posts['mission'] as $post): ?>
     <div class="about-item-bg desktop" style="background-image: url('<?php echo $post['thumb']; ?>');">
     </div>
-    <div class="container">        
+    <div class="container">
+        <?php foreach ($posts['mission'] as $post): ?>
             <div class="col-xs-12 col-md-7 about-item-content">
                 <div class="about-item-title">
                     <h2 class="text-title"><?php echo $post['title']; ?></h2>
@@ -88,17 +88,17 @@ wp_reset_query();
     </div>
   </div>
   <div class="about-item hb-green text-green" id="vision">
-    <div class="about-item-bg desktop"></div>
-    <div class="container">
-      <?php foreach ($posts['vision'] as $post): ?>
-            <div class="col-xs-12 col-md-7 about-item-content">
-                <div class="about-item-title">
-                    <h2 class="text-title"><?php echo $post['title']; ?></h2>
-                    <p><?php echo $post['description']; ?></p>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+      <div class="about-item-bg desktop"></div>
+      <div class="container">
+          <?php foreach ($posts['vision'] as $post): ?>
+              <div class="col-xs-12 col-md-7 about-item-content">
+                  <div class="about-item-title">
+                      <h2 class="text-title"><?php echo $post['title']; ?></h2>
+                      <p><?php echo $post['description']; ?></p>
+                  </div>
+              </div>
+          <?php endforeach; ?>
+      </div>
   </div>
   <div class="about-item hb-blue text-blue" id="values">
     <div class="about-item-bg desktop"></div>
